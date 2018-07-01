04f94eb8a9edf3d68f801f0bf0a974d39d1f3d2c
Anders Stølen <Anders.Stolen@emsystemer.no>
2018-07-01 10:23:41 +0200

    Forslag til frontend logikk på endring av tekst

--- src/app/components/Evanger.jsx
+++ src/app/components/Evanger.jsx
@@ -4,14 +4,34 @@ export default class Evanger extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            signedIn: false,
+            store: 'Evanger Landhandleri',
+            content1: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto reiciendis quia sint officia ut. Quasi fugiat, vero quae excepturi rerum ad cumque facilis a, illum corrupti, alias nam distinctio aspernatur!'
         }
+        this.updateText = this.updateText.bind(this);
+        this.signInOut = this.signInOut.bind(this);
     }
 
+    updateText(evt) {
+        this.setState({
+            content1: evt.target.value
+        })  
+    }
+
+    signInOut() {
+        this.state.signedIn ? this.setState({signedIn: false}) : this.setState({signedIn: true})
+    }
+
+
+
     render() {
+        const signedIn = this.state.signedIn; 
         return (
             <div>
-                <h1>Evanger landhandleri</h1>
+                <h1>{this.state.store}</h1>
+                <p>{this.state.content1}</p>
+                {signedIn ? <textarea name="textbox" id="textbox" cols="30" rows="10" value={this.state.content1} onChange={this.updateText}></textarea> : ''}
+                <button onClick={this.signInOut}>{this.state.signedIn ? 'Sign Out': 'Sign In'}</button>
             </div>
         );
     }
