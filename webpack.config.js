b51e7de364b8fc4b8427ab4e9c9edd5e39fc24d6
Desember29 <thomas.desember29@gmail.com>
2018-07-07 22:32:41 +0200

    Working Webpack loading with CSS

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ var plugins = [
         template: path.join(__dirname, 'public', 'index.html'),
         favicon: path.join(__dirname, 'public', 'favicon.ico'),
         filename: 'index.html'
-    })
+    }),
 ];
 
 var app = [
@@ -37,7 +37,15 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: 'babel-loader'
+            },
+            {
+                test: /(\.css$)/,
+                loaders: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
             }
         ]
     },
